4cfd376e1b0f1ed4b2f81cf6303c6fe1e498a768
KKS <KKS@KKS-PC>
2016-08-26 01:50:07 +0900

    회원가입부분 실행완료(테이블에 패스워드확인 부분 추가 후 수정해야함)

--- WebContent/members/membersForm.jsp
+++ WebContent/members/membersForm.jsp
@@ -1,7 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%@ taglib uri="/struts-tags" prefix="s" %>
-<%@ page import="java.util.Date" %>  
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -19,11 +19,11 @@ function list(){
 </head>
 <body>
 <center>
-<h3>사원정보등록</h3>
-<form method="post" action="http://localhost:8060/TEP/insertMembers.action" name="frm">
+<h3>회원가입 정보입력</h3>
+<form method="post" action="http://localhost:8060/TEP/successMembers.action" name="frm">
 	<table border="1" bordercolor="#ccccff" cellpadding="0" cellspacing="0" width="300" height="300">
 		<tr>
-			<td>이메일</td>
+			<td>ID(이메일)</td>
 			<td>
 				<input type="text" name="m_email">
 			</td>
@@ -64,12 +64,7 @@ function list(){
 				<input type="text" name="m_fav_field">
 			</td>
 		</tr>
-		<tr>
-			<td>가입 날짜</td>
-			<td>
-				<input type="date" name="m_date">
-			</td>
-		</tr>
+		
 		<tr>
 			<td colspan="2" align="center">
 				<input type="button" value="등록" onclick="insert()">
--- src/members/MembersAction.java
+++ src/members/MembersAction.java
@@ -5,106 +5,46 @@ import java.util.Date;
 import java.util.*;
 import config.SqlMapper;
 import java.sql.*;
-public class MembersAction {
-
-	private MembersModel vo = new MembersModel();
+public class MembersAction extends MembersModel{
 	private SqlMapClient sqlMapper;
 	
-	private String m_email;
-	private String m_name;
-	private String m_nickname;
-	private String m_password;
-	private String m_phone;
-	private String m_fav_area;
-	private String m_fav_field;
-	private Date m_date;
+	Calendar today = Calendar.getInstance();
 	
 	public MembersAction(){
 		sqlMapper = SqlMapper.getMapper();
 	}
 	
+	public String home(){
+		return "success";
+	}
 	public String Insert() throws Exception{
+		MembersModel vo = new MembersModel();
 		
-		vo.setM_email(m_email);
-		vo.setM_name(m_name);
-		vo.setM_nickname(m_nickname);
-		vo.setM_password(m_password);
-		vo.setM_phone(m_phone);
-		vo.setM_fav_area(m_fav_area);
-		vo.setM_fav_field(m_fav_field);
-		vo.setM_date(m_date);
+		vo.setM_email(getM_email());
+		vo.setM_name(getM_name());
+		vo.setM_nickname(getM_nickname());
+		vo.setM_password(getM_password());
+		vo.setM_phone(getM_phone());
+		vo.setM_fav_area(getM_fav_area());
+		vo.setM_fav_field(getM_fav_field());
+		vo.setM_date(today.getTime());
 		
 		
-		sqlMapper.insert("insertMember", vo);
+		
+		sqlMapper.insert("two.insertMember", vo);
 		
 		return "success";
 		
-	}
-
-	public String getM_email() {
-		return m_email;
-	}
-
-	public void setM_email(String m_email) {
-		this.m_email = m_email;
-	}
-
-	public String getM_name() {
-		return m_name;
-	}
-
-	public void setM_name(String m_name) {
-		this.m_name = m_name;
-	}
-
-	public String getM_nickname() {
-		return m_nickname;
-	}
-
-	public void setM_nickname(String m_nickname) {
-		this.m_nickname = m_nickname;
-	}
-
-	public String getM_password() {
-		return m_password;
-	}
-
-	public void setM_password(String m_password) {
-		this.m_password = m_password;
-	}
-
-	public String getM_phone() {
-		return m_phone;
-	}
-
-	public void setM_phone(String m_phone) {
-		this.m_phone = m_phone;
-	}
-
-	public String getM_fav_area() {
-		return m_fav_area;
-	}
-
-	public void setM_fav_area(String m_fav_area) {
-		this.m_fav_area = m_fav_area;
-	}
-
-	public String getM_fav_field() {
-		return m_fav_field;
-	}
-
-	public void setM_fav_field(String m_fav_field) {
-		this.m_fav_field = m_fav_field;
-	}
-
-	public Date getM_date() {
-		return m_date;
-	}
-
-	public void setM_date(Date m_date) {
-		this.m_date = m_date;
-	}
 	
+	}
+
+	public Calendar getToday() {
+		return today;
+	}
+
+	public void setToday(Calendar today) {
+		this.today = today;
+	}
 	
 	
 }
